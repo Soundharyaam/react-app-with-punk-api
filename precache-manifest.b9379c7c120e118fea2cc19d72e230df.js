743cc4ffdc50c645f017faf114b3fc8f8d53d664
Soundharyaam <soundharyabharatiya@gmail.com>
2019-06-09 16:38:55 +0530

    Updates

---
+++ precache-manifest.b9379c7c120e118fea2cc19d72e230df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app-with-punk-api/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fe48d8d118c8c26b2448",
+    "url": "/react-app-with-punk-api/static/js/runtime~main.fe48d8d1.js"
+  },
+  {
+    "revision": "d51c41374443059ebdc5",
+    "url": "/react-app-with-punk-api/static/js/main.d51c4137.chunk.js"
+  },
+  {
+    "revision": "525eca81e823cf3735e3",
+    "url": "/react-app-with-punk-api/static/js/2.525eca81.chunk.js"
+  },
+  {
+    "revision": "d51c41374443059ebdc5",
+    "url": "/react-app-with-punk-api/static/css/main.ad034493.chunk.css"
+  },
+  {
+    "revision": "021e2ffc80dce4e15a550f589fbf24f7",
+    "url": "/react-app-with-punk-api/index.html"
+  }
+];
